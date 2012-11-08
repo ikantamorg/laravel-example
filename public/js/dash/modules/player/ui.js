680c8b8296ed3a907453f22abc791d58c48b9d7b
Kapil <kapv89@gmail.com>
2012-11-09 03:30:27 +0530

    stuff that works

--- public/js/dash/modules/player/ui.js
+++ public/js/dash/modules/player/ui.js
@@ -6,6 +6,18 @@ define(
 		'./ui/domEventBinder'
 	],
 	function (urlGen, vent, dom, domEventBinder) {
+		var multiAnimationHelperFactory = function (cutOff) {
+			var count = 0;
+			var helper = _.extend({}, Backbone.Events);
+			helper.cutOff = cutOff;
+			helper.updateCounter = function () {
+				if(++count === helper.cutOff)
+					helper.trigger('done');
+			};
+
+			return helper;
+		};
+
 		domEventBinder.run();
 
 		var ui = {
@@ -33,16 +45,43 @@ define(
 				dom.playToggleBtn().hasClass('pause') && dom.playToggleBtn().removeClass('pause');
 			},
 
-			togglePlaylistWindow: function () {
-				if(dom.videoWindow().height() === 0) {
-					dom.playlistWindow().animate({'top': '-339px', 'height': '350px'}, 150);
-					dom.playlistScreen().animate({'top': '-339px', 'height': '315px'}, 150);
+			togglePlaylistWindow: function (callback) {
+				if(dom.videoWindow().height() > 0) {
+					return this.toggleVideoWindow(this.togglePlaylistWindow);
 				}
+
+				var animeHelper = multiAnimationHelperFactory(2);
+				var options = {duration: 150, complete: animeHelper.updateCounter};
+
+				if(dom.playlistWindow().height() > 0) {
+					dom.playlistWindow().animate({top: 12, height: 0}, options);
+					dom.playlistScreen().animate({height: 0}, options);
+				} else {
+					dom.playlistWindow().animate({top: -339, height: 350}, options);
+					dom.playlistWindow().animate({height: 315}, options);
+				}
+
+				if(typeof(callback) === 'function') animeHelper.on('done', callback);
 			},
 
-			toggleVideoWindow: function () {
+			toggleVideoWindow: function (callback) {
+				if(dom.playlistWindow().height() > 0) {
+					return this.togglePlaylistWindow(this.toggleVideoWindow);
+				}
 
+				var animeHelper = multiAnimationHelperFactory(2);
+				var options = {duration: 150, complete: animeHelper.updateCounter};
+
+				if(dom.videoWindow().height() > 0) {
+					dom.videoWindow().animate({top: 12, height: 0}, options);
+					dom.videoWindow().animate({height: 0}, options);
+				} else {
+					dom.videoWindow().animate({top: -339, height: 350}, options);
+					dom.videoWindow().animate({height: 315}, options);
+				}
+
+				if(typeof(callback) === 'function').animeHelper.on('done', callback);
 			}
-		}
+		};
 	}
 );
